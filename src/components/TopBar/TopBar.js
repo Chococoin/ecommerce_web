1dc6a8e5246b0f56495bb699f77a96e3ea487554
German Lugo <bitnibs@gmail.com>
2017-06-08 11:24:39 +0200

    WIP

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 
 import './TopBar.css';
 
+function handlerCategoryMenu(){
+  let menu = document.getElementsByClassName("CategoryMenu");
+  menu.style = "50%";
+}
+
 const TopBar = props => (
   <div className="TopBar">
-    <div className="Hamburger"/>
+    <div className="Hamburger" onClick={handlerCategoryMenu()}/>
     <div className="content">
       {props.children}
     </div>
